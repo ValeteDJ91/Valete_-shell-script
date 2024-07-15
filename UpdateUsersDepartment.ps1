03b629d6cc58209976251ad51d5fb93936f33dcf
Valete_ <valent1dj91@gmail.com>
2024-07-15 16:09:04 +0200

    Added new script to the collection
    added UpdateUsersDepartment.ps1 to the family
    YEEEAAH

---
+++ UpdateUsersDepartment.ps1
@@ -0,0 +1,11 @@
+﻿$Search = ""
+$Users = Get-ADUser -filter * -SearchBase $Search
+$progress = 0
+
+foreach ($User in $Users) {
+    Set-ADUser -Identity $User -Department "Plessis"
+
+    $progress = $progress+1
+    $pourcentage = [math]::Round($progress/$Users.Length*100,2)
+    Write-Progress -Activity "Change in Progress" -Status "$pourcentage% Complete:" -PercentComplete $pourcentage
+}
