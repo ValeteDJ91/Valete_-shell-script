a11f6e03ce843b73458df88411dc00d5f2e8cd3b
Valete_ <valent1dj91@gmail.com>
2024-09-13 12:24:52 +0200

    Ajout script pour upload ficher vers sharepoint
    Ajout de deux script pour upload des fichiers vers sharepoint

---
+++ SendFileToSharePoint.ps1
@@ -0,0 +1,14 @@
+﻿$SharepointURL = ""
+$UserName=""
+$Password = ""
+$FolderPath = ""
+$SharePointPath = ""
+ 
+$SecurePassword = ConvertTo-SecureString -String $Password -AsPlainText -Force
+$Cred = New-Object -TypeName System.Management.Automation.PSCredential -argumentlist $UserName, $SecurePassword
+
+Connect-PnPOnline -Url $SharepointURL -Credentials $Cred
+
+foreach ($file in (Get-ChildItem -Path $FolderPath | where {$_.extension -eq ".csv"})) {
+    Add-PnPFile -Folder ($SharepointURL+$SharePointPath) -Path $file.FullName
+}
---
+++ SendFileToSharePointThumbprint.ps1
@@ -0,0 +1,12 @@
+﻿$SharepointURL = ""
+$Tenant = ""
+$ClientID = ""
+$Thumbprint = ""
+$FolderPath = ""
+$SharePointPath = ""
+
+Connect-PnPOnline -Tenant $Tenant -ClientId $ClientID -Thumbprint $Thumbprint -Url $SharepointURL
+
+foreach ($file in (Get-ChildItem -Path $FolderPath | where {$_.extension -eq ".csv"})) {
+    Add-PnPFile -Folder ($SharepointURL+$SharePointPath) -Path $file.FullName
+}
